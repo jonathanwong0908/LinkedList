984758a3ba882dccacf33cc22c29da548582226a
Jonathan <ghuyers98@gmail.com>
2022-11-30 15:33:21 +0800

    add match rate indicator

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const passportSetup = require("../config/passportSetup");
 const User = require("../models/user");
 const Language = require("../models/language");
+const job = require("../models/job");
 
 exports.getLogin = (req, res) => {
     res.render("user/login");
@@ -51,9 +52,20 @@ exports.postEditProfile = async (req, res) => {
 
 exports.getJobs = async (req, res) => {
     const user = await User.findById(req.user.id);
+    const languages = await Language.find();
+    const jobs = await job.find().populate("company_id");
+    const matchingRates = [];
+    jobs.forEach(job => {
+        matchingRates.push(makeMatchingRates(user, job));
+    })
+    console.log(jobs);
+    console.log(matchingRates);
     res.render("user/jobs", {
         title: "Jobs",
-        user
+        user,
+        languages,
+        jobs,
+        matchingRates
     });
 }
 
@@ -65,7 +77,7 @@ exports.postFilterJobs = async (req, res) => {
 exports.getLogout = (req, res) => {
     req.logout((err) => {
         if (err) return err;
-        res.redirect("user/login");
+        res.redirect("/login");
     })
 }
 
@@ -97,4 +109,21 @@ function capitalizeFirstLetter(string) {
 
 function splitStringToArray(string) {
     return string.split(",");
+}
+
+function makeMatchingRates(user, job) {
+    let matchRate;
+    let count = 0;
+    for (let i = 0; i < user.language.length; i++) {
+        for (let language of job.language) {
+            if (user.language[i] === language) {
+                count++;
+            }
+        }
+    }
+    matchRate = (count / job.language.length) * 100;
+    if (matchRate >= 99 && matchRate < 100) {
+        return 99;
+    }
+    return Math.round(matchRate);
 }
--- public/scripts/jobList.js
+++ public/scripts/jobList.js
@@ -2,6 +2,10 @@ const languageFilterButton = document.querySelector(".language-selector-button")
 const languageFilterContainer = document.querySelector(".language-input-container");
 const salaryFilterButton = document.querySelector(".salary-selector-button");
 const salaryFilterContainer = document.querySelector(".salary-input-container");
+const searchLanguageInput = document.querySelector(".search-language-input");
+const languageCheckboxContainers = document.querySelectorAll(".language-checkbox-container");
+const languageCheckboxes = document.querySelectorAll(".language-checkbox");
+const matchRateIndicators = document.querySelectorAll(".match-percentage-indicator");
 
 languageFilterContainer.classList.add("hidden");
 salaryFilterContainer.classList.add("hidden");
@@ -20,4 +24,18 @@ salaryFilterButton.addEventListener("click", (event) => {
         languageFilterContainer.classList.add("hidden");
     }
     salaryFilterContainer.classList.toggle("hidden");
+})
+
+searchLanguageInput.addEventListener("input", (event) => {
+    const value = event.target.value;
+    languageCheckboxes.forEach(checkbox => {
+        const isVisible = checkbox.innerText.includes(value);
+        checkbox.closest(".language-checkbox-container").classList.toggle("hidden", !isVisible);
+    })
+})
+
+matchRateIndicators.forEach(indicator => {
+    const value = +indicator.firstElementChild.innerText.slice(0, -1);
+    const degree = (value / 100) * 360;
+    indicator.style.background = `conic-gradient(hsl(43 88% 60%) ${degree}deg, hsl(42, 89%, 89%) 0deg)`
 })
